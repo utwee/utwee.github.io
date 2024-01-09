43c3048b1be0e3020d981b860b3418d2100f42e9
Robin Müller <utwee@student.kit.edu>
2024-01-09 17:01:46 +0100

    Update camera.js

--- camera.js
+++ camera.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const constraints = {
         video: {
           facingMode: {
-            exact: 'environment'
+            exact: 'user'
           }
         }
       };
